e7046b267d33247793f6ef16949d1dd89949f134
byodian <byj6696@gmail.com>
2022-09-25 15:11:29 +0800

    chore(nvim): cmp

--- nvim/.config/nvim/lua/byodian/config/cmp.lua
+++ nvim/.config/nvim/lua/byodian/config/cmp.lua
@@ -1,187 +1,188 @@
 local has_words_before = function()
-	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 -- nvim-cmp setup
 local status_ok, luasnip = pcall(require, "luasnip")
 if not status_ok then
-	print('Note: Please install "luasnip" plugin')
-	return
+  print('Note: Please install "luasnip" plugin')
+  return
 end
 require("luasnip.loaders.from_vscode").lazy_load()
 require("luasnip.loaders.from_snipmate").lazy_load({ paths = "./snippets" })
 
 local tabnine_status_ok, tabnine = pcall(require, "cmp_tabnine.config")
 if not tabnine_status_ok then
-	print('Note: Please install "cmp_tabnine" plugin')
-	return
+  print('Note: Please install "cmp_tabnine" plugin')
+  return
 end
 
 tabnine:setup({
-	max_lines = 1000,
-	max_num_results = 20,
-	sort = true,
-	run_on_every_keystroke = true,
-	snippet_placement = "..",
-	ignored_file_types = {},
+  max_lines = 1000,
+  max_num_results = 20,
+  sort = true,
+  run_on_every_keystroke = true,
+  snippet_placement = "..",
+  ignored_file_types = {},
 })
 
 local kind_icons = {
-	Text = "",
-	Method = "",
-	Function = "",
-	Constructor = "",
-	Field = "",
-	Variable = "",
-	Class = "ﴯ",
-	Interface = "",
-	Module = "",
-	Property = "ﰠ",
-	Unit = "",
-	Value = "",
-	Enum = "",
-	Keyword = "",
-	Snippet = "",
-	Color = "",
-	File = "",
-	Reference = "",
-	Folder = "",
-	EnumMember = "",
-	Constant = "",
-	Struct = "",
-	Event = "",
-	Operator = "",
-	TypeParameter = "",
+  Text = "",
+  Method = "",
+  Function = "",
+  Constructor = "",
+  Field = "",
+  Variable = "",
+  Class = "ﴯ",
+  Interface = "",
+  Module = "",
+  Property = "ﰠ",
+  Unit = "",
+  Value = "",
+  Enum = "",
+  Keyword = "",
+  Snippet = "",
+  Color = "",
+  File = "",
+  Reference = "",
+  Folder = "",
+  EnumMember = "",
+  Constant = "",
+  Struct = "",
+  Event = "",
+  Operator = "",
+  TypeParameter = "",
 }
 
 -- Setup nvim-cmp.
 local cmp_status_ok, cmp = pcall(require, "cmp")
 if not cmp_status_ok then
-	print('Note: Please install "cmp" plugin')
-	return
+  print('Note: Please install "cmp" plugin')
+  return
 end
 
 local git_status_ok, cmp_git = pcall(require, "cmp_git")
 if not git_status_ok then
-	print('Note: Please install "cmp_git" plugin')
-	return
+  print('Note: Please install "cmp_git" plugin')
+  return
 end
 
 local npm_status_ok, cmp_npm = pcall(require, "cmp-npm")
 if not npm_status_ok then
-	print('Note: Please install "cmp-npm" plugin')
-	return
+  print('Note: Please install "cmp-npm" plugin')
+  return
 end
 
 cmp_git.setup()
 cmp_npm.setup()
 
 cmp.setup({
-	snippet = {
-		expand = function(args)
-			luasnip.lsp_expand(args.body)
-		end,
-	},
-	mapping = {
-		["<C-p>"] = cmp.mapping.select_prev_item(),
-		["<C-n>"] = cmp.mapping.select_next_item(),
-		["<C-b>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
-		["<C-Space>"] = cmp.mapping.complete(),
-		["<C-e>"] = cmp.mapping.close(),
-		["<CR>"] = cmp.mapping.confirm({
-			behavior = cmp.ConfirmBehavior.Replace,
-			select = true,
-		}),
-		["<Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expand_or_jumpable() then
-				luasnip.expand_or_jump()
-			elseif has_words_before() then
-				cmp.complete()
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
-				luasnip.jump(-1)
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-	},
-	sources = cmp.config.sources({
-		{ name = "nvim_lsp" },
-		{ name = "luasnip" },
-		{ name = "path" },
-		{
-			name = "buffer",
-			option = {
-				keyword_length = 5,
-				get_bufnrs = function()
-					return vim.api.nvim_list_bufs()
-				end,
-			},
-		},
-		{ name = "npm", keyword_length = 4 },
-		{ name = "cmp_tabnine" },
-		{ name = "git" },
-		{ name = "emoji" },
-		{ name = "nvim_lua" },
-	}),
-	formatting = {
-		-- fields = { 'abbr', 'kind' , 'menu' },
-		format = function(entry, vim_item)
-			-- Kind icons
-			vim_item.kind = string.format("%s %s", kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
-			-- Source
-			vim_item.menu = ({
-				buffer = "[Buffer]",
-				nvim_lsp = "[LSP]",
-				luasnip = "[LuaSnip]",
-				nvim_lua = "[Lua]",
-				latex_symbols = "[LaTeX]",
-				cmp_tabnine = "[TN]",
-			})[entry.source.name]
-			return vim_item
-		end,
-	},
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-d>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.close(),
+    ["<CR>"] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+  }),
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+    { name = "path" },
+    {
+      name = "buffer",
+      option = {
+        keyword_length = 5,
+        get_bufnrs = function()
+          return vim.api.nvim_list_bufs()
+        end,
+      },
+    },
+    { name = "npm", keyword_length = 4 },
+    { name = "cmp_tabnine" },
+    { name = "git" },
+    { name = "emoji" },
+    { name = "nvim_lua" },
+  }),
+  formatting = {
+    -- fields = { 'abbr', 'kind' , 'menu' },
+    format = function(entry, vim_item)
+      -- Kind icons
+      vim_item.kind = string.format("%s %s", kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+      -- Source
+      vim_item.menu = ({
+        buffer = "[Buffer]",
+        nvim_lsp = "[LSP]",
+        luasnip = "[LuaSnip]",
+        nvim_lua = "[Lua]",
+        latex_symbols = "[LaTeX]",
+        cmp_tabnine = "[TN]",
+      })[entry.source.name]
+      return vim_item
+    end,
+  },
+  experimental = {
+    ghost_text = true,
+  },
 })
 
 cmp.setup.cmdline(":", {
-	mapping = cmp.mapping.preset.cmdline(),
-	sources = cmp.config.sources({
-		{ name = "path" },
-		{ name = "cmdline" },
-	}),
-	formatting = {
-		format = function(entry, vim_item)
-			vim_item.kind = string.format("%s", kind_icons[vim_item.kind]) -- This concatonates the icons with the name of the item kind
-			vim_item.menu = ({
-				buffer = "",
-			})[entry.source.name]
-			return vim_item
-		end,
-	},
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = "path" },
+    { name = "cmdline" },
+  }),
+  formatting = {
+    format = function(entry, vim_item)
+      vim_item.kind = string.format("%s", kind_icons[vim_item.kind]) -- This concatonates the icons with the name of the item kind
+      vim_item.menu = ({
+        buffer = "",
+      })[entry.source.name]
+      return vim_item
+    end,
+  },
 })
 
 cmp.setup.cmdline("/", {
-	mapping = cmp.mapping.preset.cmdline(),
-	sources = {
-		{ name = "buffer" },
-	},
-	formatting = {
-		format = function(entry, vim_item)
-			vim_item.kind = string.format("%s", vim_item.kind) -- This concatonates the icons with the name of the item kind
-			vim_item.menu = ({
-				buffer = "",
-			})[entry.source.name]
-			return vim_item
-		end,
-	},
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = "buffer" },
+  },
+  formatting = {
+    format = function(entry, vim_item)
+      vim_item.kind = string.format("%s", vim_item.kind) -- This concatonates the icons with the name of the item kind
+      vim_item.menu = ({
+        buffer = "",
+      })[entry.source.name]
+      return vim_item
+    end,
+  },
 })
