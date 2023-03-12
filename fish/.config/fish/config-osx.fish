bdeea74e586eb9bc7159add2cf2c1a076e712f43
byodian <byj6696@gmail.com>
2023-03-12 23:01:55 +0800

    feat: tilling window manager

--- fish/.config/fish/config-osx.fish
+++ fish/.config/fish/config-osx.fish
@@ -1,6 +1,10 @@
 alias npx "sudo npx"
+alias ida "open -na \"IntelliJ IDEA CE\""
 
+set -gx M_HOME ~/Downloads/apache-maven-3.9.0
+set -gx PATH $M_HOME/bin $PATH
 set -gx PATH /opt/homebrew/bin $PATH
+set -gx STOW_FOLDERS "yabai,skhd,spacebar" $STOW_FOLDERS
 
 # set up proxy
 # set -xg http_proxy http://127.0.0.1:7890
