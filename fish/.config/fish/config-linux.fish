3ade3b000a22f23e275dc10b211af7568ff58b75
byodian <byj6696@gmail.com>
2023-03-18 10:24:25 +0800

    chore(fish): fix ansible bug

--- fish/.config/fish/config-linux.fish
+++ fish/.config/fish/config-linux.fish
@@ -10,11 +10,11 @@ alias apt-get "sudo apt-get"
 alias update "sudo apt-get update && sudo apt-get upgrade"
 
 # alacritty
-alias ala "alacritty.exe --config-file ~/.config/alacritty/alacritty.yml -e 'wsl.exe ~'"
+# alias ala "alacritty.exe --config-file ~/.config/alacritty/alacritty.yml -e 'wsl.exe ~'"
 
 # set the default node version for new shells use
 # https://github.com/jorgebucaran/nvm.fish#quickstart
-set --universal nvm_default_version (cat ~/.nvmrc)
+# set --universal nvm_default_version (cat ~/.nvmrc)
 
 # set windows system32
 # set -gx PATH /mnt/c/Windows/System32 $PATH
@@ -22,7 +22,7 @@ set --universal nvm_default_version (cat ~/.nvmrc)
 
 # set up proxy
 set HTTPIP (cat /etc/resolv.conf | grep "nameserver" | cut -f 2 -d " ")
-set PROXYHTTP "http://$HTTPIP:7890"
+set PROXYHTTP "http://$HTTPIP:1080"
 
 set -gx https_proxy $PROXYHTTP
 set -gx http_proxy $PROXYHTTP
