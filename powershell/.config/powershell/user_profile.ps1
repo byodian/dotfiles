ad4fc12f7e49f184bd5663b420842e42989d0c79
byodian <byj6696@gmail.com>
2023-03-31 11:29:24 +0800

    feat(powershell): update the powershell config

--- powershell/.config/powershell/user_profile.ps1
+++ powershell/.config/powershell/user_profile.ps1
@@ -1,26 +1,38 @@
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/robbyrussel.omp.json" | Invoke-Expression
+
 Import-Module posh-git
 Import-Module Terminal-Icons
 Import-Module z
+Import-Module git-aliases -DisableNameChecking
 
-# oh-my-posh init pwsh --config "$(brew --prefix oh-my-posh)/themes/robbyrussel.omp.json" | Invoke-Expression
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\robbyrussel.omp.json" | Invoke-Expression
-
-# PSReadLine
 Import-Module PSReadLine
+Set-PSReadLineOption -Colors @{
+  Command            = 'Magenta'
+  Number             = 'DarkGray'
+  Member             = 'DarkGray'
+  Operator           = 'DarkGray'
+  Type               = 'DarkGray'
+  Variable           = 'DarkGreen'
+  Parameter          = 'DarkGreen'
+  ContinuationPrompt = 'DarkGray'
+  Default            = 'DarkGray'
+}
 Set-PSReadLineOption -EditMode Emacs
-Set-PSReadLineOption -PredictionSource HistoryAndPlugin
-Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle InlineView
 
-# fzf
 Import-Module PSFzf
+Set-PsFzfOption -PSReadLineChordProvider 'Ctrl+t' -PSReadLineChordReverseHistory 'Ctrl+r'
 
 # Alias
 Set-Alias ll ls
 Set-Alias c clear
 Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
 Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias openssl 'C:\Program Files\Git\usr\bin\openssl.exe'
+Set-Alias idea 'idea64.exe'
 
-function which($command) {
-  Get-command -Name $command -ErrorAction SilentlyContinue | 
-    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
-}
+# powershell
+$env:HTTP_PROXY="http://127.0.0.1:1080"
+$env:HTTPS_PROXY="http://127.0.0.1:1080"
+$env:USER_PROFILE="$env:USERPROFILE\.config\powershell\user-profile.ps1"
