596a52dc973b5da4751a1a138f2dc8a06ac34509
byodian <byj6696@gmail.com>
2023-04-02 23:18:33 +0800

    feat(nvim): add undotree plugin

--- nvim-lua-config/.config/nvim/lua/byodian/config/init.lua
+++ nvim-lua-config/.config/nvim/lua/byodian/config/init.lua
@@ -3,6 +3,7 @@ require("byodian.config.basic-plugin")
 require("byodian.config.null-ls")
 require("byodian.config.nvim-treesitter")
 require("byodian.config.telescope")
+require("byodian.config.undotree")
 -- https:byodian.//github.com/neovim/nvim-lspconfig/wiki/Autocompletion
 require("byodian.config.cmp")
 require("byodian.config.gitsigns")
